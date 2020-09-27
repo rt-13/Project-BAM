897299b72f13e7ad645b9b9f79f4c2136e9614e5
Rupesh Tarwade <rupeshtarwade@outlook.com>
2020-09-27 10:44:28 +0530

    Add project files.

---
+++ BrainWorks.ATM.Data/ATMContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrainWorks.ATM.Data
+{
+	public class ATMContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			string connectionString = @"Data Source=RT-ADEPTUS\SQLEXPRESS15;Initial Catalog=MyShop;Integrated Security=True";
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+	}
+}
